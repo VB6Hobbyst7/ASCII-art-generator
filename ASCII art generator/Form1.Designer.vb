9ce3c5d990a100e3221733a48c97b221382baab0
EchoDevG <75668023+EchoDevG@users.noreply.github.com>
2021-04-26 12:44:21 +0100

    Add files via upload

---
+++ ASCII art generator/Form1.Designer.vb
@@ -0,0 +1,79 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(192, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(332, 185)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Silver
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(0, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(197, 185)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "upload image"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(25, 239)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(445, 282)
+        Me.PictureBox2.TabIndex = 2
+        Me.PictureBox2.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(521, 626)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox2 As PictureBox
+End Class
---
+++ ASCII art generator/Form1.vb
@@ -0,0 +1,38 @@
+﻿Imports System.IO
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim ofd As New OpenFileDialog
+        ofd.InitialDirectory = My.Computer.FileSystem.SpecialDirectories.MyPictures
+        ofd.Filter = "JPEG files (*.jpg)|*.jpg|Bitmap files (*.bmp)|*.bmp"
+        Dim result As DialogResult = ofd.ShowDialog
+        If Not (PictureBox1) Is Nothing And ofd.FileName <> String.Empty Then
+            PictureBox1.BackgroundImage = Image.FromFile(ofd.FileName)
+            PictureBox1.BackgroundImageLayout = ImageLayout.Zoom
+        End If
+        ImageFunctions()
+    End Sub
+
+    Sub ImageFunctions()
+        Dim bmp As Bitmap = PictureBox1.BackgroundImage
+        PictureBox2.BackgroundImage = ConvertToGreyscale(bmp)
+        PictureBox2.BackgroundImageLayout = ImageLayout.Zoom
+    End Sub
+
+    Function ConvertToGreyscale(ByVal source As Bitmap) As Bitmap
+        Dim bm As New Bitmap(source)
+        Using fp As New FastPix(bm)
+            For y As Integer = 0 To bm.Height - 1
+                For x As Integer = 0 To bm.Width - 1
+                    Dim c As Color = fp.GetPixel(x, y)
+                    Dim luma As Integer = CInt(c.R * 0.3 + c.G * 0.59 + c.B * 0.11)
+                    fp.SetPixel(x, y, Color.FromArgb(luma, luma, luma))
+                Next
+            Next
+        End Using
+        Return bm
+    End Function
+
+
+End Class
